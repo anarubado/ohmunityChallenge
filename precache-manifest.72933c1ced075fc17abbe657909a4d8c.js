ccae1c650987c23f7069e297a3e1486846d393fb
anarubado <rubadoana@gmail.com>
2020-09-21 21:28:48 -0300

    Updates

---
+++ precache-manifest.72933c1ced075fc17abbe657909a4d8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e299e39e02797c5466a1a56d2c51448",
+    "url": "/ohmunityChallenge/index.html"
+  },
+  {
+    "revision": "a150f31ce9757beef1f1",
+    "url": "/ohmunityChallenge/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "456d71986defab88ecf5",
+    "url": "/ohmunityChallenge/static/css/main.2e930aa9.chunk.css"
+  },
+  {
+    "revision": "a150f31ce9757beef1f1",
+    "url": "/ohmunityChallenge/static/js/2.79ceab88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ohmunityChallenge/static/js/2.79ceab88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456d71986defab88ecf5",
+    "url": "/ohmunityChallenge/static/js/main.a9768c1c.chunk.js"
+  },
+  {
+    "revision": "faa8cf070d464ba3f39b",
+    "url": "/ohmunityChallenge/static/js/runtime-main.3fc1e3d0.js"
+  }
+]);
